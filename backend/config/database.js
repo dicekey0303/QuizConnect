0bc2ed6cd57452712ac8274428683ba8edfe10d2
dicekey0303 <dicekey0303@gmail.com>
2024-06-06 20:33:43 +0900

    config.json 削除の調整

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('quizconnect', 'qc_user', 'qc_pass', {
-  host: 'db',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
 
